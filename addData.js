75a968fe4691a4b3be2a8854f8ed379aa18ff073
Sushant <073bct547.sushant@pcampus.edu.np>
2019-02-17 20:34:10 +0545

    this has been corrected to address the primary key problem

--- addData.js
+++ addData.js
@@ -9,14 +9,14 @@ var connection = mysql.createConnection(
 // SQL commands
 var commandToAddAuthor="INSERT INTO author (first_name,last_name,author_email) VALUES ?"
 var insertToPublication =`INSERT INTO publication(user_email,title,date,isbn,impact_factor,type_of_article,volume,name_of_journal,page_number,location_of_conference,article_link,academic_level,doi) VALUES ?`;
-var addDataInContribution = "INSERT INTO contribution (publication_id_latest,author_email) values ?" // mysql code to add in contribution table
-
+var addDataInContribution = "INSERT INTO contribution (publication_id,author_email) values ?" // mysql code to add in contribution table
+var valueInContribution=[];
 // the following are sample values. values to be entered in database should be sent as an array of objects
 var arrayOfInputObjects = [{
 	user_email:'Mosby@gmail.com',
-	title:'Best in the world',
-	date:'2020-12-12',
-	isbn:12212,
+	title:'Neon Tomorrow',
+	date:'2010-12-12',
+	isbn:12232,
 	impact_factor:2,
 	type_of_article:'academic',
 	volume:'first',
@@ -26,17 +26,16 @@ var arrayOfInputObjects = [{
 	article_link:'google.com',
 	academic_level:'Amateur',
 	doi:'this_is_doi',
-	authors:[{first_name:"Ross",last_name:"Geller",email:"rossGeller@yahoo.com"},
-			{first_name:"Ted",last_name:"Mosby",email:"tedMosby12@gmail.com"}]
+	authors:[{first_name:"Cena",last_name:"John",author_email:"johncena@gmail.com"},
+			{first_name:"Sushant",last_name:"Thapa",author_email:"thapa.sushant.ts@gmail.com"}]
 	 }]
 var arrayFromWebsite=arrayOfInputObjects[0].authors;
-
 var values = arrayOfInputObjects.map(convertToArray);
 
 
 
 var publication_id_latest;
-	
+	// add data in publication table and extract latest publication_id
 connection.query(insertToPublication,[values],function(err,result){
 		if (err) throw err;
 		console.log("publication query is done");
@@ -45,71 +44,72 @@ connection.query(insertToPublication,[values],function(err,result){
 
 	 publication_id_latest=(result[0]['max(publication_id)'])
 	 console.log("publication_id_latest is extracted")
-})	
-	})
-	//extracting the latest publication id
 
-
-
-// here comes the code to add author
 var arrayToBeAdded =[];
 		connection.query("select author_email from author",function(err,result){
 			if(err) throw err;
-
+		
 			for(let i=0;i<arrayFromWebsite.length;i++){
 				var counter=0;
+				
 				if(result.length==0) 
 					{arrayToBeAdded.push(arrayFromWebsite[i])}
 				else{
+					console.log("hi")
 				for(let j=0;j<result.length;j++)	//iterates over the array from the database
 				{
 					if(arrayFromWebsite[i].author_email==result[j].author_email)
 					{
+						console.log("the array from website is "+arrayFromWebsite[i].author_email)
+						console.log("the checking author in database is "+ result[j].author_email)
 						counter++;
 					}
-					
+					// note	
 				}
 				if(counter==0)
 					arrayToBeAdded.push(arrayFromWebsite[i])
 			}
 				}
-				
+				console.log(arrayToBeAdded)
 				var newArray = arrayToBeAdded.map(convertToArray);
 				
 				if(arrayToBeAdded.length!=0){
+					console.log("authors to be added are ")
+					console.log(arrayToBeAdded)
 			connection.query(commandToAddAuthor,[newArray],function(err,result){
 				if(err) throw err;
 				console.log("author added successfully")
-				// the following array is for contribution table
+				
+				for(k=0;k<arrayFromWebsite.length;k++)
+				{
+					valueInContribution[k]=[publication_id_latest,arrayFromWebsite[k].author_email]
+				}
+				connection.query(addDataInContribution,[valueInContribution],function(err,result){
+					if(err) throw err;
+					console.log("data added in contribution");
+				})
+				
 
-				var arrayToBeQueriedInContribution =[];
-for (let i=0;i<arrayFromWebsite.length;i++)
-{
-	var tempArray = [publication_id_latest,arrayFromWebsite[i].author_email]
-	arrayToBeQueriedInContribution.push(tempArray)
-}
+				 })
+		}
+		else
+		{
 
-		// this is the query in the contribution query
+				for(k=0;k<arrayFromWebsite.length;k++)
+				{
+					valueInContribution[k]=[publication_id_latest,arrayFromWebsite[k].author_email]
+				}
+				connection.query(addDataInContribution,[valueInContribution],function(err,result){
+					if(err) throw err;
+					console.log("data added in contribution");
+				})
+		}
 
-		connection.query(addDataInContribution,[arrayToBeQueriedInContribution],function(err,result){
-	console.log("contribution query is over");
+	})
+
+		})	
 })
 
-		
-			})
-		}
-		})
-
-// part of author table is over
-
-//the following is to add to contribution table
-
-
-
-//adding in the contribution table
-
-
-// contribution query over
 function convertToArray(suppliedObject)
 {
 	var result =[]

169278afc8aee61f0de52fc09c72b57d4f46d23a
Sushant <073bct547.sushant@pcampus.edu.np>
2019-07-22 17:37:25 +0545

    the type of issn has been changed from int to text

--- createTables.js
+++ createTables.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql'); // anallgous to #include in c/c++;
+	var mysql = require('mysql'); // anallgous to #include in c/c++;
 var connection  = mysql.createConnection(
 {
 	host:'localhost',
@@ -16,10 +16,10 @@ var commandCreateContributionTable = "CREATE TABLE contribution(publication_id I
 // publication acts as the base class from which the others are derived
 var commandCreatePublicationTable= "CREATE TABLE publication (publication_id INT AUTO_INCREMENT PRIMARY KEY,user_email VARCHAR(50), title TEXT, year INT, impact_factor FLOAT,FOREIGN KEY(user_email) REFERENCES user(user_email))";
 
-var commandCreateReportTable = "CREATE TABLE report (publication_id INT,department TEXT,institute TEXT, report_type TEXT, pages INT, ISSN INT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id))"
+var commandCreateReportTable = "CREATE TABLE report (publication_id INT,department TEXT,institute TEXT, report_type TEXT, pages INT, ISSN TEXT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id))"
 var commandCreateProceedingTable ="CREATE TABLE proceeding (publication_id INT,country TEXT, editor TEXT,conference_publication_name TEXT, month TEXT, page_number TEXT,FOREIGN KEY (publication_id) REFERENCES publication(publication_id))"
-var commandCreateJournalTable = "CREATE TABLE journal (publication_id INT,editor TEXT, volume FLOAT, journal_name TEXT,page_number TEXT, month TEXT, day TEXT,issue TEXT, ISSN int, FOREIGN KEY (publication_id) REFERENCES publication(publication_id))"
-var commandCreateArticleTable = "CREATE TABLE article(publication_id INT,editor TEXT,volume FLOAT,periodical_title TEXT,month TEXT, day TEXT,pages INT,issue TEXT,ISSN INT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id))"
+var commandCreateJournalTable = "CREATE TABLE journal (publication_id INT,editor TEXT, volume FLOAT, journal_name TEXT,page_number TEXT, month TEXT, day TEXT,issue TEXT, ISSN TEXT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id))"
+var commandCreateArticleTable = "CREATE TABLE article(publication_id INT,editor TEXT,volume FLOAT,periodical_title TEXT,month TEXT, day TEXT,pages INT,issue TEXT,ISSN TEXT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id))"
 var commandCreateBookTable = "CREATE TABLE book(publication_id INT,country TEXT,editor TEXT,volume INT,edition INT,ISBN TEXT,Page_number TEXT,Chapter_number INT,Book_author TEXT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id))"
 // following is an array of all the SQL commands
 var tableCommands = [commandCreateUserTable,

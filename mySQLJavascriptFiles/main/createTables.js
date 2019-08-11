fe104a32e908c91622c55d4e231e05046781eb9f
Sushant <073bct547.sushant@pcampus.edu.np>
2019-08-11 10:31:29 +0545

    The create table has been updated to reflect that all attribut names are not lowercase and mutliple words are separated by underscore. Also, author_email is not set to Not Null

--- mySQLJavascriptFiles/main/createTables.js
+++ mySQLJavascriptFiles/main/createTables.js
@@ -9,20 +9,20 @@ var connection  = mysql.createConnection(
 
 // following are the list of sql commands for various table creation properties;
 var commandCreateUserTable ="CREATE TABLE user(user_email VARCHAR(50),first_name TEXT, last_name TEXT,department TEXT,password TEXT, PRIMARY KEY(user_email))"
-var commandCreateAuthorTable="CREATE TABLE author(author_email VARCHAR(50),author_id INT AUTO_INCREMENT,first_name TEXT,last_name TEXT,PRIMARY KEY(author_id))"; 
+var commandCreateAuthorTable="CREATE TABLE author(author_email VARCHAR(50) UNIQUE NOT NULL,author_id INT AUTO_INCREMENT,first_name TEXT,last_name TEXT,PRIMARY KEY(author_id))"; 
 // publication acts as the base class from which the others are derived
 
-var commandCreatePublicationTable= "CREATE TABLE publication (publication_id INT AUTO_INCREMENT PRIMARY KEY,user_email VARCHAR(50), title VARCHAR(200) UNIQUE, year INT, publisher TEXT,impact_factor FLOAT,city TEXT,doi TEXT,ranked BOOLEAN,rank TEXT,type_of_publication TEXT,FOREIGN KEY(user_email) REFERENCES user(user_email) ON DELETE CASCADE)";
+var commandCreatePublicationTable= "CREATE TABLE publication (publication_id INT AUTO_INCREMENT PRIMARY KEY,user_email VARCHAR(50), title VARCHAR(200), year INT, publisher TEXT,impact_factor FLOAT,city TEXT,doi TEXT,ranked BOOLEAN,rank TEXT,type_of_publication TEXT,FOREIGN KEY(user_email) REFERENCES user(user_email) ON DELETE CASCADE,UNIQUE(user_email,title))";
 // the following is the contribution table, which serves as the bridge table
 var commandCreateContributionTable = "CREATE TABLE contribution(publication_id INT, author_id INT, FOREIGN KEY(author_id) REFERENCES author(author_id) ON DELETE CASCADE,FOREIGN KEY(publication_id) references publication(publication_id) ON DELETE CASCADE)"
 // now for the actual entries
 
 
-var commandCreateReportTable = "CREATE TABLE report (publication_id INT,department TEXT,institute TEXT, report_type TEXT, pages INT, ISSN TEXT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id) ON DELETE CASCADE)"
+var commandCreateReportTable = "CREATE TABLE report (publication_id INT,department TEXT,institute TEXT, report_type TEXT, pages INT, issn TEXT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id) ON DELETE CASCADE)"
 var commandCreateProceedingTable ="CREATE TABLE proceeding (publication_id INT,country TEXT, editor TEXT,conference_publication_name TEXT, month TEXT, page_number TEXT,FOREIGN KEY (publication_id) REFERENCES publication(publication_id) ON DELETE CASCADE)"
-var commandCreateJournalTable = "CREATE TABLE journal (publication_id INT,editor TEXT, volume FLOAT, journal_name TEXT,page_number TEXT, month TEXT, day TEXT,issue TEXT, ISSN TEXT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id) ON DELETE CASCADE)"
-var commandCreateArticleTable = "CREATE TABLE article(publication_id INT,editor TEXT,volume FLOAT,periodical_title TEXT,month TEXT, day TEXT,pages INT,issue TEXT,ISSN TEXT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id) ON DELETE CASCADE)"
-var commandCreateBookTable = "CREATE TABLE book(publication_id INT,country TEXT,editor TEXT,volume INT,edition INT,ISBN TEXT,Page_number TEXT,Chapter_number INT,Book_author TEXT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id) ON DELETE CASCADE)"
+var commandCreateJournalTable = "CREATE TABLE journal (publication_id INT,editor TEXT, volume FLOAT, journal_name TEXT,page_number TEXT, month TEXT, day TEXT,issue TEXT, issn TEXT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id) ON DELETE CASCADE)"
+var commandCreateArticleTable = "CREATE TABLE article(publication_id INT,editor TEXT,volume FLOAT,periodical_title TEXT,month TEXT, day TEXT,pages INT,issue TEXT,issn TEXT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id) ON DELETE CASCADE)"
+var commandCreateBookTable = "CREATE TABLE book(publication_id INT,country TEXT,editor TEXT,volume INT,edition INT,isbn TEXT,page_number TEXT,chapter_number INT, book_author TEXT, FOREIGN KEY (publication_id) REFERENCES publication(publication_id) ON DELETE CASCADE)"
 // following is an array of all the SQL commands
 var tableCommands = [commandCreateUserTable,
 commandCreateAuthorTable,
